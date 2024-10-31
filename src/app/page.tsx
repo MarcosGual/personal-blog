fba35136a5b4e11459ef2086e7c54558d7032863
MarcosGual <marcos.gual@outlook.com>
2024-10-31 02:05:57 -0300

    add: images + responsive body css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,19 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Link href="/">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quam nemo ab doloremque ipsum officia asperiores error? Adipisci doloribus placeat rerum corporis, ipsam facere culpa dolorum, deserunt nemo voluptatem vel?</h1>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus vitae veritatis quisquam reiciendis temporibus illum deserunt quam. Sequi, consequatur omnis vel optio cupiditate tempore iusto velit quo? Culpa, veritatis optio!
+      Sunt eveniet incidunt doloremque ipsa nulla expedita cupiditate blanditiis quod aperiam aut, sint deleniti dignissimos quam doloribus at! Modi recusandae saepe iure? Saepe dignissimos obcaecati mollitia animi iusto magnam distinctio!
+      Et consequatur natus corporis cumque ea. Debitis magnam perspiciatis quo quasi sed vel illo maxime autem itaque! Nemo repellat corporis ex! Repudiandae laudantium itaque esse exercitationem perspiciatis nobis et amet!
+      Non provident quas impedit. Magni blanditiis corporis, voluptates nemo optio fugiat iusto dolorem facilis commodi! Eum fuga expedita at facilis ut neque eos amet, voluptatibus esse, dolore quo laudantium dolor.
+      Amet accusantium ad expedita soluta, iure possimus animi dignissimos commodi vero itaque quaerat voluptatem. Asperiores atque nulla placeat maiores molestias. Quaerat temporibus fugiat exercitationem architecto vero facere, earum illo ullam?
+      Quas quo adipisci reiciendis suscipit aliquam officiis quaerat ipsum, nulla commodi praesentium porro doloremque sequi dolores vero impedit sed tempore culpa quibusdam odio nihil consequatur? Temporibus quia suscipit doloremque rerum?
+      Deleniti dolorum error sunt, et assumenda tempora accusamus eligendi ut eum ratione praesentium, architecto incidunt ea! Eius voluptas, vitae nemo blanditiis, nisi consequatur eligendi ratione quidem minima dolorem fugiat quisquam?
+      Eligendi aliquam omnis nulla voluptatem architecto consectetur molestias voluptates, voluptatum id recusandae? Ratione vel minus corporis perferendis corrupti recusandae dolorem maxime aut cupiditate nemo, odit dignissimos quo provident officia commodi?
+      Atque ea quisquam officiis, totam nemo dolores ipsa nisi, alias dolorum quo debitis. Amet expedita voluptas officia voluptates beatae architecto temporibus perferendis eveniet error voluptatibus. Eligendi suscipit earum saepe aliquid.
+      Tempore tempora a harum, quas placeat obcaecati dolore ipsam nam velit eveniet perspiciatis repellendus reprehenderit nihil, id neque quasi atque impedit maxime, perferendis quia? A eos commodi error ducimus consequatur.
+    </Link>
   );
 }
