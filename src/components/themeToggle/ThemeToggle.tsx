5a19b4fa9338e0255b372cc76347a5643fdff98d
MarcosGual <marcos.gual@outlook.com>
2024-11-02 20:55:50 -0300

    add: toggle (theme-toggles)

--- src/components/themeToggle/ThemeToggle.tsx
+++ src/components/themeToggle/ThemeToggle.tsx
@@ -1,7 +1,14 @@
+"use client"
+import { useState } from "react";
 import styles from "./themeToggle.module.css";
+import { DarkSide } from "@theme-toggles/react"
 
 const ThemeToggle: React.FC = () => {
-    return <div>toggle</div>
+    const [isToggled, setToggle] = useState(false)
+
+    return (<div>
+        <DarkSide duration={750} toggled={isToggled} toggle={setToggle} />
+    </div>)
 }
 
 export default ThemeToggle;
