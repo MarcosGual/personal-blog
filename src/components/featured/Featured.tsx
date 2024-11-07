3d2d8327b9e141b4b9fb2637d1610cafb3a9f3b3
MarcosGual <marcos.gual@outlook.com>
2024-11-07 20:45:29 -0300

    add: featured section

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
 import styles from "./featured.module.css";
 
-const Featured: React.FC = ()=>{
-    return <div className={styles.container}>Destacados</div>;
+const Featured: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                <b>Hola, soy Marcos!</b> Descubrí historias e ideas creativas.
+            </h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src="/p1.jpeg" alt="" className={styles.image} fill />
+                </div>
+                <div className={styles.textContainer}>
+                    <h2 className={styles.postTitle}>Lorem ipsum dolor sit amet.</h2>
+                    <p className={styles.postPreview}>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Vitae perspiciatis quam est quod debitis nam dicta praesentium
+                        sit ea quisquam asperiores harum in quia amet itaque,
+                        quidem quaerat at repudiandae.
+                    </p>
+                    <button className={styles.button}>Leer más...</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Featured;
