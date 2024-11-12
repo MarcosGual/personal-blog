d9e1fb6cd35bd953d2745ce8b843d2fe1e0d9bc7
MarcosGual <marcos.gual@outlook.com>
2024-11-11 22:28:36 -0300

    add: menuposts component + styles

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -13,15 +13,126 @@ const Menu: React.FC = () => {
                         <Image src='/p1.jpeg' alt="" fill className={styles.image} />
                     </div>
                     <div className={styles.textContainer}>
-                        <span className={`${styles.category} ${styles.travel}`}>
-                            Viajes
+                        <span className={`${styles.category} ${styles.economics}`}>
+                            Economía
                         </span>
                         <h3>
                             Lorem ipsum dolor sit amet consectetur.
                         </h3>
                         <div className={styles.detail}>
                             <span className={styles.username}>Juan Perez - </span>
-                            <span className={styles.date}>10.03.2024</span>
+                            <span className={styles.date}>10.11.2024</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.imageContainer}>
+                        <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.history}`}>
+                            Historia
+                        </span>
+                        <h3>
+                            Lorem ipsum dolor sit amet consectetur.
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>Juan Perez - </span>
+                            <span className={styles.date}>23.07.2024</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.imageContainer}>
+                        <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.coding}`}>
+                            Programación
+                        </span>
+                        <h3>
+                            Lorem ipsum dolor sit amet consectetur.
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>Juan Perez - </span>
+                            <span className={styles.date}>28.04.2024</span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+            <h3 className={styles.subtitle}>{"descubrí por temas 📖"}</h3>
+            <h2 className={styles.title}>Categorías</h2>
+            <div className={styles.categoryList}>
+                <Link href='/blog?cat=economics' className={`${styles.categoryItem} ${styles.economics}`}>
+                    Economía
+                </Link>
+                <Link href='/blog?cat=politics' className={`${styles.categoryItem} ${styles.politics}`}>
+                    Política
+                </Link>
+                <Link href='/blog?cat=history' className={`${styles.categoryItem} ${styles.history}`}>
+                    Historia
+                </Link>
+                <Link href='/blog?cat=coding' className={`${styles.categoryItem} ${styles.coding}`}>
+                    Programación
+                </Link>
+                <Link href='/blog?cat=football' className={`${styles.categoryItem} ${styles.football}`}>
+                    Fuchibol
+                </Link>
+                <Link href='/blog?cat=anything' className={`${styles.categoryItem} ${styles.anything}`}>
+                    Cosas
+                </Link>
+            </div>
+            <h3 className={styles.subtitle}>{"elegido por el 🔎"}</h3>
+            <h2 className={styles.title}>Editor</h2>
+            <div className={styles.items}>
+                <Link href="/" className={styles.item}>
+                    <div className={styles.imageContainer}>
+                        <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.economics}`}>
+                            Economía
+                        </span>
+                        <h3>
+                            Lorem ipsum dolor sit amet consectetur.
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>Juan Perez - </span>
+                            <span className={styles.date}>10.11.2024</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.imageContainer}>
+                        <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.history}`}>
+                            Historia
+                        </span>
+                        <h3>
+                            Lorem ipsum dolor sit amet consectetur.
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>Juan Perez - </span>
+                            <span className={styles.date}>23.07.2024</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.imageContainer}>
+                        <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.coding}`}>
+                            Programación
+                        </span>
+                        <h3>
+                            Lorem ipsum dolor sit amet consectetur.
+                        </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>Juan Perez - </span>
+                            <span className={styles.date}>28.04.2024</span>
                         </div>
                     </div>
                 </Link>
---
+++ src/components/menuPosts/MenuPosts.tsx
@@ -0,0 +1,14 @@
+import styles from './menuPosts.module.css';
+
+interface MenuPostsProps {
+    withImage: string;
+}
+
+const MenuPosts: React.FC<MenuPostsProps> = ({ withImage }) => {
+    return (<div>
+        MenuPosts
+    </div>
+    );
+}
+
+export default MenuPosts;
