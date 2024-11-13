3d74443fdf55c8ab0b5168fd8d0ab4d01f44940b
MarcosGual <marcos.gual@outlook.com>
2024-11-13 00:19:06 -0300

    add: footer component

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,7 +1,14 @@
 import styles from "./pagination.module.css";
 
-const Pagination: React.FC = ()=>{
-    return <div className={styles.container}>Paginación</div>
+const Pagination: React.FC = () => {
+    return <div className={styles.container}>
+        <button className={styles.button}>
+            Anterior
+        </button>
+        <button className={styles.button}>
+            Siguiente
+        </button>
+    </div>
 }
 
 export default Pagination;
