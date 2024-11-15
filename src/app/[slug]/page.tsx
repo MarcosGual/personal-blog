ff74ff5d01a090d01b223530e59ec2fc70751c35
MarcosGual <marcos.gual@outlook.com>
2024-11-15 14:48:12 -0300

    add: singlePage + basic styles

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,32 @@
+import Menu from '@/components/Menu/Menu';
 import styles from './singlePage.module.css';
+import Image from 'next/image';
 
 const SinglePage: React.FC = () => {
     return (
         <div className={styles.container}>
-            <div></div>
+            <div className={styles.infoContainer}>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.title}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <div className={styles.user}>
+                        <div className={styles.userImageContainer}>
+                            <Image src="/p1.jpeg" alt='' fill className={styles.image} />
+                        </div>
+                        <div className={styles.userTextContainer}>
+                            <span className={styles.username}>Juan Perez</span>
+                            <span className={styles.date}>10.11.2024</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image src="/p1.jpeg" alt='ejemplo' fill className={styles.image} />
+                </div>
+            </div>
+
+            <div className={styles.content}>
+                <div className={styles.post}></div>
+                <Menu />
+            </div>
         </div>
     )
 }
