c5c41a01ccd6201bd86faed60deeca89b73aafbe
MarcosGual <marcos.gual@outlook.com>
2024-11-21 12:35:21 -0300

    add: finishing comments section styles

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -25,7 +25,7 @@ const Comments = () => {
         <div className={styles.comment}>
           <div className={styles.user}>
             <Image
-              src="/p1.jpg"
+              src="/p1.jpeg"
               alt="imagen usuario"
               className={styles.image}
               width={50}
@@ -36,6 +36,57 @@ const Comments = () => {
               <span className={styles.date}>01.10.24</span>
             </div>
           </div>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+            debitis quas nostrum deserunt veritatis? Ipsam ut molestiae dolorem
+            hic similique cupiditate dicta recusandae quo esse sunt, repudiandae
+            unde fugiat non harum ea autem modi deserunt doloribus quae
+            quibusdam velit voluptatibus!
+          </p>
+        </div>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt="imagen usuario"
+              className={styles.image}
+              width={50}
+              height={50}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>Juan Pérez</span>
+              <span className={styles.date}>01.10.24</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+            debitis quas nostrum deserunt veritatis? Ipsam ut molestiae dolorem
+            hic similique cupiditate dicta recusandae quo esse sunt, repudiandae
+            unde fugiat non harum ea autem modi deserunt doloribus quae
+            quibusdam velit voluptatibus!
+          </p>
+        </div>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt="imagen usuario"
+              className={styles.image}
+              width={50}
+              height={50}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>Juan Pérez</span>
+              <span className={styles.date}>01.10.24</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+            debitis quas nostrum deserunt veritatis? Ipsam ut molestiae dolorem
+            hic similique cupiditate dicta recusandae quo esse sunt, repudiandae
+            unde fugiat non harum ea autem modi deserunt doloribus quae
+            quibusdam velit voluptatibus!
+          </p>
         </div>
       </div>
     </div>
