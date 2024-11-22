067360811cf3d998a6b9b6e1763459a1128011b6
MarcosGual <marcos.gual@outlook.com>
2024-11-22 00:03:03 -0300

    add: write page styles

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -2,31 +2,49 @@
 import Image from "next/image";
 import styles from "./write.module.css";
 import { useState } from "react";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.bubble.css";
 
 const WritePage: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("");
 
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="título" />
+      <input type="text" placeholder="título" className={styles.input} />
       <div className={styles.editor}>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={()=>setOpen(!open)}>
           <Image src="/plus.png" alt="agregar post" width={16} height={16} />
         </button>
       </div>
       {open && (
         <div className={styles.add}>
           <button className={styles.addButton}>
-            <Image src="/image.png" alt="agregar post" width={16} height={16} />
+            <Image src="/image.png" 
+            alt="agregar post" 
+            width={16} 
+            height={16} 
+            />
           </button>
           <button className={styles.addButton}>
-            <Image src="/external.png" alt="agregar post" width={16} height={16} />
+            <Image
+              src="/external.png"
+              alt="agregar post"
+              width={16}
+              height={16}
+            />
           </button>
           <button className={styles.addButton}>
             <Image src="/video.png" alt="agregar post" width={16} height={16} />
           </button>
         </div>
       )}
+      <ReactQuill
+        theme="bubble"
+        value={value}
+        onChange={setValue}
+        placeholder="escribí tu historia..."
+      />
     </div>
   );
 };
