4f758afa4a0c09ed20ca4c90484d15ddfefc69c2
MarcosGual <marcos.gual@outlook.com>
2024-11-25 18:38:52 -0300

    add: google authentication

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,15 @@
+'use client'
+import { signIn, useSession } from 'next-auth/react';
 import styles from './loginPage.module.css';
 
 const LoginPage: React.FC = ()=>{
+    const {data, status} = useSession();
+    console.log(data, status);
+
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-                <div className={styles.socialButton}>Ingresar con Google</div>
+                <div className={styles.socialButton} onClick={()=>signIn('google')}>Ingresar con Google</div>
                 <div className={styles.socialButton}>Ingresar con Github</div>
                 <div className={styles.socialButton}>Ingresar con Facebook</div>
             </div>
