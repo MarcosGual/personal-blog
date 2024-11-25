087beed0e36941a609e9a33903bb7a0749bc8f38
MarcosGual <marcos.gual@outlook.com>
2024-11-24 23:50:19 -0300

    add: logo link

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,23 +5,32 @@ import AuthLinks from "../authLinks/AuthLinks";
 import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar: React.FC = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.social}>
-                <Image src="/facebook.png" alt="facebook" width={24} height={24} />
-                <Image src="/instagram.png" alt="instagram" width={24} height={24} />
-                <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
-                <Image src="/youtube.png" alt="youtube" width={24} height={24} />
-            </div>
-            <div className={styles.logo}>programación pragmática</div>
-            <div className={styles.links}>
-                <ThemeToggle />
-                <Link className={styles.link} href="">Inicio</Link>
-                <Link className={styles.link} href="">Contacto</Link>
-                <Link className={styles.link} href="">Acerca de</Link>
-                <AuthLinks />
-            </div>
-        </div>)
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+      </div>
+      <Link href="/" className={styles.logo}>
+        <div>programación pragmática</div>
+      </Link>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link className={styles.link} href="">
+          Inicio
+        </Link>
+        <Link className={styles.link} href="">
+          Contacto
+        </Link>
+        <Link className={styles.link} href="">
+          Acerca de
+        </Link>
+        <AuthLinks />
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
