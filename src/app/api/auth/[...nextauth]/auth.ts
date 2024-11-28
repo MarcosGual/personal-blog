c5ca52ba29ab9dc56da74f5e60fe7abb38b5e39d
MarcosGual <marcos.gual@outlook.com>
2024-11-27 23:55:58 -0300

    add: schema changes

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,8 +1,10 @@
-
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Prisma } from "@prisma/client";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(Prisma),
   providers: [GitHub, Google],
-})
+});
