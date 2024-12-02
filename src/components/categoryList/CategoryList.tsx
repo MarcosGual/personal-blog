1093f94ef13135de7b5d064e244b2f9af48915d2
MarcosGual <marcos.gual@outlook.com>
2024-12-02 16:24:32 -0300

    add: get category data

--- src/components/categoryList/CategoryList.tsx
+++ src/components/categoryList/CategoryList.tsx
@@ -2,74 +2,50 @@ import Link from "next/link";
 import styles from "./categoryList.module.css";
 import Image from "next/image";
 
-const CategoryList: React.FC = () => {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Categorías Populares</h1>
-            <div className={styles.categories}>
-                <Link href="blog?cat=style" className={`${styles.category} ${styles.football}`}>
-                    <Image
-                        src="/style.png"
-                        alt=""
-                        width={32}
-                        height={32}
-                        className={styles.image}
-                    />
-                    fuchibol
-                </Link>
-                <Link href="blog?cat=style" className={`${styles.category} ${styles.history}`}>
-                    <Image
-                        src="/fashion.png"
-                        alt=""
-                        width={32}
-                        height={32}
-                        className={styles.image}
-                    />
-                    historia
-                </Link>
-                <Link href="blog?cat=style" className={`${styles.category} ${styles.politics}`}>
-                    <Image
-                        src="/food.png"
-                        alt=""
-                        width={32}
-                        height={32}
-                        className={styles.image}
-                    />
-                    política
-                </Link>
-                <Link href="blog?cat=style" className={`${styles.category} ${styles.economics}`}>
-                    <Image
-                        src="/travel.png"
-                        alt=""
-                        width={32}
-                        height={32}
-                        className={styles.image}
-                    />
-                    economía
-                </Link>
-                <Link href="blog?cat=style" className={`${styles.category} ${styles.anything}`}>
-                    <Image
-                        src="/culture.png"
-                        alt=""
-                        width={32}
-                        height={32}
-                        className={styles.image}
-                    />
-                    cosas
-                </Link>
-                <Link href="blog?cat=style" className={`${styles.category} ${styles.coding}`}>
-                    <Image
-                        src="/coding.png"
-                        alt=""
-                        width={32}
-                        height={32}
-                        className={styles.image}
-                    />
-                    programación
-                </Link>
-            </div>
-        </div>
-    );
+interface Category {
+  id: string;
+  slug: string;
+  title: string;
+  img: string;
 }
+
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed at getting data");
+  }
+
+  return res.json();
+};
+
+const CategoryList: React.FC = async () => {
+  const data = await getData();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Categorías Populares</h1>
+      <div className={styles.categories}>
+        {data?.map((category: Category) => (
+          <Link
+            href="blog?cat=style"
+            className={`${styles.category} ${styles[category?.title]}`}
+          >
+            {category.img && <Image
+              src={category.img}
+              alt=""
+              width={32}
+              height={32}
+              className={styles.image}
+            />}
+            {category.slug}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CategoryList;
