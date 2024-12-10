8bd22465ba1aa6d404a431763cdbadf34a0209fc
MarcosGual <marcos.gual@outlook.com>
2024-12-10 19:50:02 -0300

    add: get single post + user info

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,64 +2,61 @@ import Menu from "@/components/Menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
+import { getData } from "@/utils/data";
+import { Params } from "@/app/api/posts/[slug]/route";
+import { dateTimeToDate } from "@/utils/functions";
 
-const SinglePage: React.FC = () => {
+const SinglePage: React.FC<Params> = async ({ params }: Params) => {
+  const searchParams = await params;
+
+  let { post } = await getData(`/posts/${searchParams.slug}`);
+  // console.log(post);
+
+  if (!post || post === "undefined") {
+    return <div>Cargando...</div>;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          </h1>
+          <h1 className={styles.title}>{post.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
-            </div>
+            {post?.author.image && (
+              <div className={styles.userImageContainer}>
+                <Image
+                  src={post.author.image}
+                  alt=""
+                  fill
+                  className={styles.avatar}
+                />
+              </div>
+            )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>Juan Perez</span>
-              <span className={styles.date}>10.11.2024</span>
+              <span className={styles.username}>{post?.author.name}</span>
+              <span className={styles.date}>
+                {dateTimeToDate(post.createdAt)}
+              </span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          <Image src="/p1.jpeg" alt="ejemplo" fill className={styles.image} />
-        </div>
+        {post.img && (
+          <div className={styles.imageContainer}>
+            <Image
+              src={post.img || "/p1.jpeg"}
+              alt="imagen del posteo"
+              fill
+              className={styles.image}
+            />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.description}>
-            <h5>Lorem ipsum dolor sit amet.</h5>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
-              praesentium, repellendus quam sunt dolores quidem natus delectus
-              exercitationem quo in sapiente consectetur excepturi debitis hic
-              cumque, sed nulla fugit. Maxime.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
-              nemo voluptatum sint amet culpa id. Nesciunt asperiores hic
-              dolorem nemo, culpa inventore debitis ipsum optio officiis saepe
-              doloribus ipsa modi itaque, totam odio sequi, quibusdam facilis.
-              Debitis quisquam similique labore.
-            </p>
-            <h5>Lorem ipsum dolor sit.</h5>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam
-              magni nesciunt magnam inventore sit dolorum commodi voluptatem
-              eligendi sed totam, id obcaecati distinctio vel velit neque
-              consequatur voluptatibus similique doloribus odit officiis
-              incidunt beatae et. Autem, repudiandae saepe facere cumque dolor
-              numquam blanditiis odit excepturi qui optio quod deleniti rerum!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
-              iure cumque ullam recusandae iusto ea rerum, cum, officiis
-              quibusdam dolor adipisci sint voluptatem velit ab exercitationem
-              debitis nesciunt fuga autem tempore. Nisi omnis vitae, eum
-              necessitatibus, sapiente dolorem repellendus sit animi dignissimos
-              enim, quas odio.
-            </p>
-          </div>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: post?.desc }}
+          />
           <div className={styles.comment}>
             <Comments />
           </div>
